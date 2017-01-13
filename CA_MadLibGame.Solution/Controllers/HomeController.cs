76ff62bd1b685df164e8f0cc0f85fffc047d230b
John Velis <john@johnvelis.com>
2017-01-13 16:43:03 -0500

    commit442

--- CA_MadLibGame.Solution/Controllers/HomeController.cs
+++ CA_MadLibGame.Solution/Controllers/HomeController.cs
@@ -11,10 +11,6 @@ namespace CA_MadLibGame.Controllers
     {
         public ActionResult Index()
         {
-
-
-
-
             return View();
         }
 
@@ -23,13 +19,12 @@ namespace CA_MadLibGame.Controllers
             StringBuilder storyText = new StringBuilder();
 
             storyText.Clear();
-            storyText.Append("<p>");
             storyText.Append($"You are my {1} love.You have me completely in your {2}.");
 
 
-            I know and feel that if I am to 3 anything fine and noble in the future I shall do so only by 4 at the 5 of your heart.I would like to go through life 6 by 6 with you, 7 you more and more until we 8 to be 9 being(s) together until the hour should come for us to 10.
+            //I know and feel that if I am to 3 anything fine and noble in the future I shall do so only by 4 at the 5 of your heart.I would like to go through life 6 by 6 with you, 7 you more and more until we 8 to be 9 being(s) together until the hour should come for us to 10.
 
-                     ViewBag.Message = storyText.ToString();
+            ViewBag.StoryText = storyText.ToString();
 
             return View();
         }
